b9464e736ba39e930d525a2f9a81a1cbfef85450
Steven <steven@entityzero.com>
2015-06-01 07:54:39 +1200

    Door: Added overload Open/Close
    Now can open/close a list of doors

--- YASEL/Helpers/Door.cs
+++ YASEL/Helpers/Door.cs
@@ -20,11 +20,19 @@ namespace Door
                 throw new Exception("Door.Open: Null Argument");
             door.GetActionWithName("Open_On").Apply(door);
         }
+        static  public void Open(List<IMyTerminalBlock> doors)
+        {
+            doors.ForEach(door => { Door.Open(door as IMyDoor); });
+        }
         static public void Close(IMyDoor door)
         {
             if (door == null)
                 throw new Exception("Door.Close: Null Argument");
             door.GetActionWithName("Open_Off").Apply(door);
+        }
+        static public void Close(List<IMyTerminalBlock> doors)
+        {
+            doors.ForEach(door => { Door.Close(door as IMyDoor); });
         }
         static public bool IsClosed(List<IMyTerminalBlock> doors)
         {
