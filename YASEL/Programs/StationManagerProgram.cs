d74faa6a730c6b208d3cd691183f1cefda317e03
Steven <steven@entityzero.com>
2015-06-01 14:51:06 +1200

    StationManagerProgram: Removed Airlock Code

--- YASEL/Programs/StationManagerProgram.cs
+++ YASEL/Programs/StationManagerProgram.cs
@@ -13,7 +13,7 @@ namespace StationManagerProgram
 {
     using StationManager;
     using Grid;
-    using TextPanel;
+    
     class StationManagerProgram : Program.Program
     {
         StationManager myStationManager;
@@ -24,7 +24,7 @@ namespace StationManagerProgram
             Grid.Set(GridTerminalSystem, Me, Echo);
 
             if (myStationManager==null)
-                myStationManager = new StationManager(new StationManagerSettings() { TextPanelTimeName = "TP Time", OnAirlockUpdate = AirlockLog });
+                myStationManager = new StationManager(new StationManagerSettings() { TextPanelTimeName = "TP Time"});
 
             if (TimeSinceRun.TotalMilliseconds < 250)
             {
@@ -34,13 +34,8 @@ namespace StationManagerProgram
             TimeSinceRun = new TimeSpan();
             myStationManager.ManageAutoDoors();
             myStationManager.DisplayTime();
-            myStationManager.ManageAirlocks();
         }
 
-        void AirlockLog(string name, string state, float percent)
-        {
-            TextPanel.Write("Airlock "+name+" TP", "Airlock: " + name + "\nState: " + state + "\nOxygen Level: " + percent*100 + "%", false);
-        }
     }
     
 }
