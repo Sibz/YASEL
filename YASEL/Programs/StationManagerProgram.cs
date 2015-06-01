a02e4354bab1570d97427cff2b667c978e58b246
Steven <steven@entityzero.com>
2015-06-01 12:57:54 +1200

    StationManagerProgram: Added AirlockLog function
    To enable viewing of airlock status

--- YASEL/Programs/StationManagerProgram.cs
+++ YASEL/Programs/StationManagerProgram.cs
@@ -13,19 +13,26 @@ namespace StationManagerProgram
 {
     using StationManager;
     using Grid;
+    using TextPanel;
     class StationManagerProgram : Program.Program
     {
         StationManager myStationManager;
+        
         void Main(string argument)
         {
             Grid.Set(GridTerminalSystem, Me, Echo);
 
             if (myStationManager==null)
-                myStationManager = new StationManager(new StationManagerSettings() { TextPanelTimeName = "TP Time" });
+                myStationManager = new StationManager(new StationManagerSettings() { TextPanelTimeName = "TP Time", OnAirlockUpdate = AirlockLog });
 
             myStationManager.ManageAutoDoors();
             myStationManager.DisplayTime();
             myStationManager.ManageAirlocks();
         }
+        void AirlockLog(string name, string state, float percent)
+        {
+            TextPanel.Write("Airlock "+name+" TP", "Airlock: " + name + "\nState: " + state + "\nOxygen Level: " + percent*100 + "%", false);
+        }
     }
+    
 }
