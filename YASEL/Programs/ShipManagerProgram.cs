0f9067f06f61953b7baaa153a991db8c80f79deb
Steven <steven@entityzero.com>
2015-11-01 20:45:46 +1300

    ShipManger - Reverted shipmanagerprogram
    to basic example

--- YASEL/Programs/ShipManagerProgram.cs
+++ YASEL/Programs/ShipManagerProgram.cs
@@ -16,35 +16,20 @@ namespace ShipManagerProgram
 
     class ShipManagerProgram : MyGridProgram
     {
-        static ShipManager sm;
 
         void Main(string argument)
         {
             Grid.Set(this);
 
-            if (sm == null)
-                sm = new ShipManager(new ShipManagerSettings() { LoadFromGroupName = "OreShipCargo", LoadToGroupName = "Js Cargo" });
+
+            ShipManager sm = new ShipManager();
 
             if (argument == "")
             {
-                sm.ManageDockingState("JS Base Connector", true, true, false);
-                sm.ManageBreachDoors("JS Air Vent - Hangar 1", "JS Door - BridgeToHangar", "JS Door - HangarToBridge", "JS Air Vent - Bridge");
-                sm.LoadFromGroup();
-                sm.Tick();
+                sm.ManageDockingState();
             }
             else if (argument == "undock" || argument == "dock")
                 sm.Dock();
-            else if (argument.Contains("Airlock"))
-            {
-                string [] airlockArg = argument.Split(' ');
-                if (airlockArg.Length<3)
-                {
-                    Echo("Error Open/Close Airlock: Invalid Argument");
-                    throw new Exception();
-                    //return;
-                }
-                sm.SwitchAirlockNonPressurised(airlockArg[2], airlockArg[0]=="Open");
-            }
         }
     }
 }
