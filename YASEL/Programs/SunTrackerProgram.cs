abf5e523c4ecb8deabeef6f147120cf7c37db50f
Steven <steven@entityzero.com>
2015-11-19 00:08:11 +1300

    Added SunTrackerProgram

---
+++ YASEL/Programs/SunTrackerProgram.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using VRageMath;
+
+namespace SunTrackerProgram
+{
+    using ProgramExtensions;
+    using SunTracker;
+    using SolarExtensions;
+    using BatteryManager;
+    using BatteryExtensions;
+    using BlockExtensions;
+
+    class SunTrackerProgram : MyGridProgram
+    {
+
+        SunTracker mySunTracker;
+        BatteryManager myBatteryManager;
+        IMySolarPanel refPanel;
+        List<IMyTerminalBlock> reactors;
+        
+
+
+        void Main(string argument)
+        {
+            if (mySunTracker==null)
+            {
+                refPanel = this.GetBlock("Solar Panel Test", false) as IMySolarPanel;
+                mySunTracker = new SunTracker(this);
+                mySunTracker.AddSolarArray("Solar Panel Test", "Rotor 2 - Array 1",-0.1f);
+                reactors = new List<IMyTerminalBlock>();
+                GridTerminalSystem.GetBlocksOfType<IMyReactor>(reactors, b => { return b.CubeGrid == Me.CubeGrid; });
+            }
+            mySunTracker.TrackSun();
+            myBatteryManager = new BatteryManager(this, false);
+            if (refPanel.GetMaxPowerOutput()==0)
+            {
+                // During the night, set station batteries to auto, and ship batteries to recharge
+                myBatteryManager.Batteries.ForEach(b => { if (b.CubeGrid == Me.CubeGrid) (b as IMyBatteryBlock).Recharge(false); else (b as IMyBatteryBlock).Recharge(); });
+            }
+            else 
+            {
+                // During the day, turn on the reactor and set batteries to recharge. This will utilise 100% solar energy, and top up the rest will reactor power.
+                reactors.TurnOn(); 
+                myBatteryManager.Recharge();
+            }
+        }
+
+    }
+   
+}
