0efc0bba64b37478cd6bbce9a4e61152c60709b0
Steven <steven@entityzero.com>
2016-09-19 19:50:47 +1200

    Added timer extensions for Trigger/Start

---
+++ YASEL/Extensions/TimerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerExtensions
+{
+    public static class TimerExtensions
+    {
+        public static void Trigger(this IMyTimerBlock timer)
+        {
+            timer.GetActionWithName("TriggerNow").Apply(timer);
+        }
+        public static void Start(this IMyTimerBlock timer)
+        {
+            timer.GetActionWithName("Start").Apply(timer);
+        }
+    }
+   
+}
